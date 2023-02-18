4f9d2c871b9d4df1a9e4c52f22792ed1a2763052
Ihor Borkaliuk <happyviolence1990@gmail.com>
2023-02-18 14:51:10 +0100

    added login

--- src/routes/auth/usersRouter.js
+++ src/routes/auth/usersRouter.js
@@ -6,10 +6,9 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get("/", userController.current);
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
-router.post("/logout", authMiddleware, userController.logout);
-router.post("/current", authMiddleware, userController.current);
+router.get("/logout", authMiddleware, userController.logout);
+router.get("/current", authMiddleware, userController.current);
 
 module.exports = router;
