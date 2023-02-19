c54aba7c8c738d68762392d2b32433cbd54ac9ce
Ihor Borkaliuk <happyviolence1990@gmail.com>
2023-02-19 18:48:33 +0100

    seems like google works

--- src/controllers/users/google.js
+++ src/controllers/users/google.js
@@ -1,13 +1,14 @@
 const queryString = require("querystring");
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, BASE_URL, FRONTEND_URL } = process.env;
 const { User } = require("../../schemas/user");
+const { v4: uuidv4 } = require("uuid");
 
 const googleAuth = async (req, res) => {
   const stringifiedParams = queryString.stringify({
     client_id: process.env.GOOGLE_CLIENT_ID,
-    redirect_uri: `${process.env.BASE_URL}/auth/users/google-redirect`,
+    redirect_uri: `${BASE_URL}/auth/users/google-redirect`,
     scope: [
       "https://www.googleapis.com/auth/userinfo.email",
       "https://www.googleapis.com/auth/userinfo.profile",
@@ -25,16 +26,16 @@ const googleRedirect = async (req, res) => {
   const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
   const urlObj = new URL(fullUrl);
   const urlParams = queryString.parse(urlObj.search);
-  const code = urlParams.code;
+  const code = req.query.code || req.body.code || req.params.code;
   const tokenData = await axios({
     url: `https://oauth2.googleapis.com/token`,
     method: "post",
     data: {
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.BASE_URL}/auth/users/google-redirect`,
+      redirect_uri: `${BASE_URL}/auth/users/google-redirect`,
       grant_type: "authorization_code",
-      code,
+      code: code,
     },
   });
 
@@ -56,11 +57,10 @@ const googleRedirect = async (req, res) => {
     await User.findByIdAndUpdate(user._id, { token });
     return res.redirect(`${FRONTEND_URL}?token=${token}`);
   } else {
-    const avatar = gravatar.url(email);
     const newUser = await User.create({
       email,
-      password: hashedPassword,
-      avatarURL: avatar,
+      password: uuidv4(),
+      verificationToken: uuidv4(),
       verify: true,
     });
     const payload = {
@@ -69,7 +69,7 @@ const googleRedirect = async (req, res) => {
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
     await User.findByIdAndUpdate(newUser._id, { token });
 
-    return res.redirect(`${process.env.FRONTEND_URL}?token=${token}`);
+    return res.redirect(`${FRONTEND_URL}?token=${token}`);
   }
 };
 module.exports = {
