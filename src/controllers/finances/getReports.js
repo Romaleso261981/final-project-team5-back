666252b623f613a42f33445641f0338f8c7ba533
JaRcomGitHub <jarcom@ukr.net>
2023-02-24 21:28:15 +0200

    Update getReports.js

--- src/controllers/finances/getReports.js
+++ src/controllers/finances/getReports.js
@@ -1,7 +1,41 @@
-const { User } = require("../../schemas/user");
+const { Finance } = require("../../schemas/finance");
+const { Types } = require("mongoose");
 
 async function getReports(req, res) {
-  return res.status(501).json({ message: "getReports" });
+  const owner = req.user.id;
+  // form = 1 responce("all transactions in one massive")
+  // form = 2 responce("all transactions in one massive")
+  const { form = 1, month = 0, year = 0 } = req.query;
+  const searchParam = {
+    owner: Types.ObjectId(owner),
+    // type,
+    month: parseInt(month),
+    year: parseInt(year),
+  };
+  console.log("getReports searchParam", searchParam);
+
+  try {
+    if (form == 1) {
+      const result = await Finance.find(searchParam);
+      return res.status(200).json(result);
+    }
+
+    // const result = await Finance.aggregate([
+    //   { $match: searchParam },
+    //   { $project: { amount: 1, owner: 1 } },
+    //   { $group: { _id: "amount", amount: { $sum: "$amount" } } },
+    // ]);
+    // console.log("getSummary result", result.length, result);
+
+    // if (result.length === 1) {
+    //   const { amount } = result[0];
+    //   return res.status(200).json({ ...searchParam, amount });
+    // }
+
+    return res.status(200).json({ message: "getReports ok, ну почти" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
 
 module.exports = getReports;
