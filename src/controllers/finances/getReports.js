7d4ef8916a68d821c1f6795cbf29736834fe37b6
JaRcomGitHub <jarcom@ukr.net>
2023-02-25 14:13:33 +0200

    update reports api

--- src/controllers/finances/getReports.js
+++ src/controllers/finances/getReports.js
@@ -5,42 +5,73 @@ async function getReports(req, res) {
   const owner = req.user.id;
   // form = 1 responce("all transactions in one massive")
   // form = 2 responce("all transactions in one massive")
-  const { form = 1, month = 0, year = 0 } = req.query;
+  // type: "0" or "expenses" or "income"
+  const {
+    total = 1,
+    type = "0",
+    category = "",
+    month = 0,
+    year = 0,
+  } = req.query;
   const searchParam = {
     owner: Types.ObjectId(owner),
-    // type,
     month: parseInt(month),
     year: parseInt(year),
   };
+  console.log("req.query", req.query);
   console.log("getReports searchParam", searchParam);
 
   try {
-    if (form == 1) {
-      const result = await Finance.find(searchParam);
+    if (total == 1 && type === "0") {
+      const result = await Finance.find(searchParam).select({
+        owner: 0,
+        __v: 0,
+      });
       return res.status(200).json(result);
-    } else if (form == 2) {
+    } else if (total == 2 && type === "0") {
       const result = await Finance.aggregate([
         { $match: searchParam },
-        // { $project: { type: 1 } },
-        { $sort: { month: 1 } },
+        { $project: { amount: 1, type: 1 } },
+        {
+          $group: {
+            _id: { type: "$type" },
+            totalAmount: { $sum: "$amount" },
+          },
+        },
+        { $sort: { _id: 1 } },
       ]);
-
+      return res.status(200).json(result);
+    } else if (
+      total == 1 &&
+      (type === "expenses" || type === "income") &&
+      category === ""
+    ) {
+      const result = await Finance.aggregate([
+        { $match: { ...searchParam, type } },
+        { $project: { amount: 1, category: 1 } },
+        {
+          $group: {
+            _id: { category: "$category" },
+            totalAmount: { $sum: "$amount" },
+          },
+        },
+        { $sort: { totalAmount: -1 } },
+      ]);
+      return res.status(200).json(result);
+    } else if (
+      total == 1 &&
+      (type === "expenses" || type === "income") &&
+      category !== ""
+    ) {
+      const result = await Finance.aggregate([
+        { $match: { ...searchParam, type, category } },
+        { $project: { amount: 1, category: 1, description: 1 } },
+        { $sort: { amount: -1 } },
+      ]);
       return res.status(200).json(result);
     }
 
-    // const result = await Finance.aggregate([
-    //   { $match: searchParam },
-    //   { $project: { amount: 1, owner: 1 } },
-    //   { $group: { _id: "amount", amount: { $sum: "$amount" } } },
-    // ]);
-    // console.log("getSummary result", result.length, result);
-
-    // if (result.length === 1) {
-    //   const { amount } = result[0];
-    //   return res.status(200).json({ ...searchParam, amount });
-    // }
-
-    return res.status(200).json({ message: "getReports ok, ну почти" });
+    return res.status(200).json({ message: "Reports return empty" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
