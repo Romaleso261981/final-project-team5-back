caa8531fc3a80013a45d065140f10c53f2e4e06a
Ihor Borkaliuk <happyviolence1990@gmail.com>
2023-02-27 19:19:29 +0100

    Update login.js

--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -10,38 +10,40 @@ async function login(req, res, next) {
     const { email, password } = req.body;
     const { error } = validateRegisterSchema.validate(req.body);
     if (error) {
-      res.status(400).json({ message: "Wrong email or password" });
+      return res.status(400).json({ message: "Wrong email or password" });
     }
 
     const user = await User.findOne({ email });
     const userPassword = await bcrypt.compare(password, user.password);
     if (!user || !userPassword) {
-      res.status(401).json({ message: "Email or password is wrong" });
+      return res.status(401).json({ message: "Email or password is wrong" });
     }
 
     if (!user.verify) {
-      res.json({ message: "Your Email is not verifyied!" });
+      return res.status(401).json({ message: "Your Email is not verifyied!" });
     }
 
     const payload = {
       id: user._id,
     };
-    const token = jwt.sign(payload, ACCESS_SECRET_KEY, { expiresIn: "2m" });
+    const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, {
+      expiresIn: "2m",
+    });
     const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, {
       expiresIn: "7d",
     });
-    await User.findByIdAndUpdate(user._id, { token, refreshToken });
-    res.json({
+    await User.findByIdAndUpdate(user._id, { accessToken, refreshToken });
+    res.status(200).json({
       status: "success",
       code: 200,
-      token: token,
+      accessToken: accessToken,
       refreshToken: refreshToken,
       user: {
         email: user.email,
       },
     });
   } catch (error) {
-    next(error);
+    return res.status(500).json({ message: error.message });
   }
 }
 
